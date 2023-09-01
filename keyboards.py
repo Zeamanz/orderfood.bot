3198d8377836eae31863b13b1c04a8e5574cb5aa
Zeamanz <135522897+Zeamanz@users.noreply.github.com>
2023-09-01 23:38:51 +0300

    Update keyboards.py

--- keyboards.py
+++ keyboards.py
@@ -1,7 +1,7 @@
 from aiogram.types import WebAppInfo
 from aiogram import types
 
-web_app = WebAppInfo(url='site_url')
+web_app = WebAppInfo(url='https://zeamanz.github.io/orderfood.bot/')
 
 keyboard = types.ReplyKeyboardMarkup(
     keyboard=[
